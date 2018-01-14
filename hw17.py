929ea4853eccae8d177348cd8101f42eb0a809f8
ADMIN <sergey.shyshkin@gmail.com>
2018-01-14 22:45:08 +0200

    correction for variable names

--- hw17.py
+++ hw17.py
@@ -1,14 +1,8 @@
 def solve_quadratic_equation(a, b, c): #returns 2 values: either 2 roots, 1 root and none, 2 Nones
-    D = b*b - 4*a*c
-    if D > 0:
-        return ((-b + D**0.5)/ (2*a), (-b - D**0.5)/ (2*a))
-    elif D == 0:
+    discrim = b*b - 4*a*c
+    if discrim > 0:
+        return ((-b + discrim**0.5)/ (2*a), (-b - discrim**0.5)/ (2*a))
+    elif discrim == 0:
         return (-b / (2*a), None)
     else:
         return None, None
-
-a=10
-b=2
-c=0
-
-print(solve_quadratic_equation(a, b, c))
