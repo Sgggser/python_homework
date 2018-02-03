6856d4c74077b298437fc3e3017cb821f1093fa8
Sgggser <34405070+Sgggser@users.noreply.github.com>
2018-02-03 18:42:42 +0200

    Add files via upload
    1. Corrected for variable name
    2. Corrected function for "magic number"

--- hw25.py
+++ hw25.py
@@ -1,13 +1,17 @@
 import random
 
-def shuffle_list(list_to_shuf): # no return (shuffles list in place)
-    for _ in range(200):
+
+def shuffle_list(list_to_shuffle, n):
+    counter = 0
+    while counter < n:
+        counter += 1
         idx_1 = random.randint(0, 49)
         idx_2 = random.randint(0, 49)
-        list_to_shuf[idx_1], list_to_shuf[idx_2] = list_to_shuf[idx_2], list_to_shuf[idx_1]
+        list_to_shuffle[idx_1], list_to_shuffle[idx_2] = list_to_shuffle[idx_2], list_to_shuffle[idx_1]
     pass
+
 
 list_odd_num = [i for i in range(1, 100) if i % 2 == 1]
 print(list_odd_num)
-shuffle_list(list_odd_num)
+shuffle_list(list_odd_num, 250)
 print(list_odd_num)
